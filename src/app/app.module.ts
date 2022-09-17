5aafcd1361ab904ae6325c0c578a89a6cf072b30
vanessa <machado.vanessa32@gmail.com>
2022-09-17 18:13:01 -0300

    Ajustes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrimeiroComponenteComponent } from './primeiro-componente/primeiro-componente.component';
+import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
+import { MultiplicaPorPipe } from './multiplica-por.pipe';
+import { PaperCustomizadoComponent } from './paper-customizado/paper-customizado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrimeiroComponenteComponent
+    PrimeiroComponenteComponent,
+    SegundoComponenteComponent,
+    MultiplicaPorPipe,
+    PaperCustomizadoComponent
   ],
   imports: [
     BrowserModule,
