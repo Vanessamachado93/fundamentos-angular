4a16a359c0eeb2135497546184db4efb9e612b78
vanessa <machado.vanessa32@gmail.com>
2022-09-19 16:17:25 -0300

    Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { MultiplicaPorPipe } from './multiplica-por.pipe';
 import { PaperCustomizadoComponent } from './paper-customizado/paper-customizado.component';
 import { RenderizandoListasComponent } from './renderizando-listas/renderizando-listas.component';
 import { ComponentePersonalizadoComponent } from './componente-personalizado/componente-personalizado.component';
+import { ComponentePaiComponent } from './componente-pai/componente-pai.component';
+import { ComponenteFilhoComponent } from './componente-filho/componente-filho.component';
+import { ExemploServicos1Component } from './exemplo-servicos1/exemplo-servicos1.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,10 @@ import { ComponentePersonalizadoComponent } from './componente-personalizado/com
     MultiplicaPorPipe,
     PaperCustomizadoComponent,
     RenderizandoListasComponent,
-    ComponentePersonalizadoComponent
+    ComponentePersonalizadoComponent,
+    ComponentePaiComponent,
+    ComponenteFilhoComponent,
+    ExemploServicos1Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/exemplo-servicos1/exemplo-servicos1.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplo-servicos1',
+  templateUrl: './exemplo-servicos1.component.html',
+  styleUrls: ['./exemplo-servicos1.component.css']
+})
+export class ExemploServicos1Component  {
+  nome = "";  
+}
